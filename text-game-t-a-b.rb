c7289cb20b4f60518351d86057fa461bcbbdae54
Bpompa95 <brianpompa02@gmail.com>
2021-07-29 11:05:30 -0700

    did what i can

---
+++ text-game-t-a-b.rb
@@ -0,0 +1,19 @@
+    puts "brand new adventure
+     character with clothes bag of 10gp
+     where would you want to start
+     woods
+     store
+     castle" 
+
+
+     
+ puts "store
+ sword = 5gp
+ bow = 8gp
+ helmet = 2gp
+ bracers = 1gp
+ shield = 5gp"
+    
+    
+
+    
